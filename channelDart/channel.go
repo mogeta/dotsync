d87bf8f4931e63bbbbb66bdd6297f3066e91b674
mimori hiroshi <mimori.hiroshi@butterfly-corp.jp>
2013-03-12 17:24:28 +0900

    first commit

---
+++ channelDart/channel.go
@@ -0,0 +1,45 @@
+package channelExample
+
+import (
+	"html/template"
+	"net/http"
+	"time"
+
+	"appengine"
+	"appengine/channel"
+	"appengine/user"
+)
+
+func init() {
+	http.HandleFunc("/", main)
+	http.HandleFunc("/receive", receive)
+}
+
+var mainTemplate = template.Must(template.ParseFiles("channelDart/main.html"))
+
+func main(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c) // assumes 'login: required' set in app.yaml
+	key := r.FormValue("gamekey")
+	tok, err := channel.Create(c, u.ID+key)
+	if err != nil {
+		http.Error(w, "Couldn't create Channel", http.StatusInternalServerError)
+		c.Errorf("channel.Create: %v", err)
+		return
+	}
+
+	err = mainTemplate.Execute(w, map[string]string{
+		"token":    tok,
+		"me":       u.ID,
+		"game_key": key,
+	})
+	if err != nil {
+		c.Errorf("mainTemplate: %v", err)
+	}
+}
+
+func receive(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	key := r.FormValue("g")
+	channel.Send(c, key, "go receive!"+time.Now().String())
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+Sample of channelAPI using go and dart 
+
+
+dev_appserver.py --port=9999 channelDart
+
+http://127.0.0.1:9999
+
+see also
+
+* http://pub.dartlang.org/
+* http://pub.dartlang.org/packages/appengine_channel
+* http://dart-lang.github.com/js-interop/docs/js.html
+
